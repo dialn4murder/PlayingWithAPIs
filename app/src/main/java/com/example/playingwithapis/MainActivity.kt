88a7c50cb285042f597dd8ea7566087b63f3b0dc
niah-stack <dylanchampion596@hotmail.com>
2025-01-25 00:20:01 +0000

    Added quotes

--- app/src/main/java/com/example/playingwithapis/MainActivity.kt
+++ app/src/main/java/com/example/playingwithapis/MainActivity.kt
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.playingwithapis.databinding.ActivityMainBinding
+import org.json.JSONArray
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.Scanner
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,5 +28,57 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.button.setOnClickListener(){
+            getQuote()
+        }
+
+    }
+
+    private fun fetchData(urlString: String){
+        val thread = Thread{
+            try{
+                val url = URL(urlString)
+                val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                connection.connectTimeout = 10000
+                connection.readTimeout = 10000
+                connection.requestMethod = "GET"
+                connection.connect()
+
+                val responseCode = connection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK){
+                    val scanner = Scanner(connection.inputStream).useDelimiter("\\A")
+                    val text = if (scanner.hasNext()) scanner.next() else ""
+
+                    val quote = processQuoteJson(text)
+                    updateTextView(quote)
+                }else{
+                    updateTextView("The server has returned an error: $responseCode")
+                }
+
+            } catch (e: IOException) {
+                updateTextView("An error has occurred from the server")
+            }
+
+        }
+        thread.start()
+    }
+
+    private fun updateTextView(text: String){
+        // Updates the UI as you cant update via threads
+        runOnUiThread{
+            binding.textView.text = text
+        }
+    }
+
+    private fun getQuote(){
+        // Calls fetchData with the API
+        fetchData("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+    }
+
+    private fun processQuoteJson (jsonString: String) : String{
+        // Converts the json from the API to a regular string
+        val jsonArray = JSONArray(jsonString)
+        return jsonArray[0].toString()
     }
 }
